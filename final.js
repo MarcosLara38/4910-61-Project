c339511a28e5646edac3ca3c3e46ab184d018443
Mauricio Vargas <mvgl910@gmail.com>
2022-03-03 21:55:33 -0800

    Added javascript file and added input to home

---
+++ final.js
@@ -0,0 +1,9 @@
+const list = document.getElementById("list");
+
+var itemcounter = 0;
+
+function addingredient(){
+    var input = document.getElementById("input").value; 
+    console.log(input);
+
+}
--- home.php
+++ home.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="home.css">
+    <script src="final.js"></script>
     <title>Document</title>
 </head>
 <body>
@@ -14,5 +15,20 @@
 
     <img src="pics/banner.png">   
     <h1>This is a heading for the home page</h1>
+    <div>
+        <div>
+            <ul id = "list">
+
+            </ul>
+
+            <div class = "additem">
+                <input type = "text" id = "input" placeholder="Add ingredient...">
+                <button type = "button" id = "button" onclick="addingredient()">Add ingredient</button>
+                <button type = "button" id = "clear" onclick = "clearlist()">Clear ingredient list</button>
+            </div>
+
+
+        </div>
+    </div>
 </body>
 </html>
